862273ed4996e05db4d2a676937676a6cdd673ad
gillsinaglass <rdgill95@gmail.com>
2019-04-02 17:20:52 -0400

    Working relationship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :user_skills
+  has_many :skills, through: :user_skills
   has_many :teams
   has_many :works, through: :teams
-  has_many :collaborations, through: :works 
+  has_many :collaborations, through: :works
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Users
+User.destroy_all
+Skill.destroy_all
+Collaboration.destroy_all
+Work.destroy_all
+Team.destroy_all
+UserSkill.destroy_all
+WorkSkill.destroy_all
+
+
+
 ryan = User.create(name: "Ryan", email: "rdgill95@gmail.com", profile_picture: "https://www.facebook.com/photo.php?fbid=10215782449468165&set=a.1518396333824&type=3&source=11&referrer_profile_id=1651994391", job_title: "Developer", user_bio: "Looking for a job", git_hub: "https://github.com/gillsinaglass", cover_image: "https://scontent-iad3-1.xx.fbcdn.net/v/t31.0-8/17240262_10211152534963196_7657082779556508117_o.jpg?_nc_cat=111&_nc_ht=scontent-iad3-1.xx&oh=0ab8e395fa8a4e718c215c44760ad72d&oe=5D3CD853")
 
 kyle = User.create(name: "Kyle", email: "Kylel95@gmail.com", profile_picture: "https://images.sk-static.com/images/media/profile_images/artists/4634/huge_avatar", job_title: "Designer", user_bio: "Have A Job", git_hub: "https://github.com/gillsinaglass")
@@ -35,3 +45,9 @@ UserSkill.create(user_id: ryan.id, skill_id: ruby.id)
 UserSkill.create(user_id: ryan.id, skill_id: react.id)
 UserSkill.create(user_id: ryan.id, skill_id: javascript.id)
 UserSkill.create(user_id: kyle.id, skill_id: photoshop.id)
+
+#WorkSkills
+WorkSkill.create(skill_id: ruby.id, work_id: work1.id )
+WorkSkill.create(skill_id: react.id, work_id: work1.id)
+WorkSkill.create(skill_id: react.id, work_id: work2.id)
+WorkSkill.create(skill_id: photoshop.id, work_id: work3.id)
