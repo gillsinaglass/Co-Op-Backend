63829170d3c7b0e095eb372a3c85c5b21fbfb9ad
gillsinaglass <rdgill95@gmail.com>
2019-04-09 11:02:24 -0400

    Serialized Info

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,3 @@
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :name, :email, :profile_picture, :job_title, :user_bio, :git_hub, :instagram, :medium, :cover_image, :teams, :skills, :collaborations, :works
+end
