e8d9ba2c3a8f9e55e4be48348598698bcf980e76
gillsinaglass <rdgill95@gmail.com>
2019-04-14 16:37:11 -0400

    Starting Trello Board

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -3,4 +3,8 @@ class Collaboration < ApplicationRecord
   has_many :teams, through: :works
   has_many :users, through: :teams
   belongs_to :user
+
+  def users_uniq
+      self.users.uniq
+  end
 end
--- app/serializers/collaboration_serializer.rb
+++ app/serializers/collaboration_serializer.rb
@@ -1,3 +1,4 @@
 class CollaborationSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :git_hub, :pitch, :status, :image_one, :image_two, :image_three, :video, :works, :user, :users, :teams
+  attributes :id, :name, :description, :git_hub, :pitch, :status, :image_one, :image_two, :image_three, :video, :works, :user, :users_uniq, :teams
+
 end
